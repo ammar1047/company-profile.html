240b4df4bc13d8a62507ef248f709cbbeeb172a9
ammar1047 <if23.muhammadfauzan@mhs.ubpkarwawang.ac.id>
2024-10-16 06:35:32 +0700

    ok

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const loginForm = document.getElementById('login-form');
     const loginBtn = document.getElementById('login-btn');
- 
+
     loginForm.addEventListener('submit', function (e) {
         e.preventDefault();
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
- 
+
         if (email !== '' && password !== '') {
-            // Simulate login success
+
             alert('Login berhasil!');
             window.location.href = 'index.html';
         } else {
             alert('Silakan isi email dan password!');
         }
     });
- });
+});
